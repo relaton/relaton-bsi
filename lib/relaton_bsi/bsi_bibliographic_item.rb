b27a5cfca4604d5bb012d569893029608f75e20a
Andrei Kislichenko <android.2net@gmail.com>
2023-02-10 10:20:39 -0500

    fix issue metanorma/metanorma-bsi#190

--- lib/relaton_bsi/bsi_bibliographic_item.rb
+++ lib/relaton_bsi/bsi_bibliographic_item.rb
@@ -9,13 +9,7 @@ module RelatonBsi
 
     SUBDOCTYPES = %w[specification method-of-test method-of-specifying vocabulary code-of-practice].freeze
 
-    # @params price_code [String, nil]
-    # @param cen_processing [Boolean, nil]
     def initialize(**args) # rubocop:disable Metrics/AbcSize
-      # if args[:doctype] && !TYPES.include?(args[:doctype])
-      #   warn "[relaton-bsi] WARNING: invalid doctype: #{args[:doctype]}"
-      #   warn "[relaton-bsi] Allowed doctypes are: #{TYPES.join(', ')}"
-      # end
       if args[:subdoctype] && !SUBDOCTYPES.include?(args[:subdoctype])
         warn "[relaton-bsi] WARNING: invalid subdoctype: #{args[:subdoctype]}"
         warn "[relaton-bsi] Allowed subdoctypes are: #{SUBDOCTYPES.join(', ')}"
@@ -37,21 +31,22 @@ module RelatonBsi
     # @option opts [Boolean] :bibdata
     # @option opts [String] :lang language
     # @return [String] XML
-    def to_xml(**opts) # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/MethodLength,Metrics/PerceivedComplexity
-      super(**opts) do |b|
-        if opts[:bibdata] && (has_ext_attrs? || price_code || !cen_processing.nil?)
-          ext = b.ext do
-            b.doctype doctype if doctype
-            b.horizontal horizontal unless horizontal.nil?
-            editorialgroup&.to_xml b
-            ics.each { |i| i.to_xml b }
-            structuredidentifier&.to_xml b
-            b.stagename stagename if stagename
-          end
-          ext["schema-version"] = ext_schema unless opts[:embeded]
-        end
-      end
-    end
+    # def to_xml(**opts) # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/MethodLength,Metrics/PerceivedComplexity
+    #   super(**opts) do |b|
+    #     if opts[:bibdata] && (has_ext_attrs? || price_code || !cen_processing.nil?)
+    #       ext = b.ext do
+    #         b.doctype doctype if doctype
+    #         b.docsubtype subdoctype if subdoctype
+    #         # b.horizontal horizontal unless horizontal.nil?
+    #         editorialgroup&.to_xml b
+    #         ics.each { |i| i.to_xml b }
+    #         structuredidentifier&.to_xml b
+    #         b.stagename stagename if stagename
+    #       end
+    #       ext["schema-version"] = ext_schema unless opts[:embeded]
+    #     end
+    #   end
+    # end
 
     # @param hash [Hash]
     # @return [RelatonBsi::BsiBibliographicItem]
