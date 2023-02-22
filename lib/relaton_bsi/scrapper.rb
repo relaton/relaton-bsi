ee7d396da66da70a0ab10c63c170cee88b34711f
Andrei Kislichenko <android.2net@gmail.com>
2023-02-22 11:25:17 -0500

    fix issue #20

--- lib/relaton_bsi/scrapper.rb
+++ lib/relaton_bsi/scrapper.rb
@@ -76,7 +76,7 @@ module RelatonBsi
           fetched: Date.today.to_s,
           type: "standard",
           docid: fetch_docid(hit.hit[:code], data),
-          docnumber: hit.hit[:code].match(/\d+\S+/).to_s,
+          docnumber: hit.hit[:code].match(/\d+/).to_s,
           language: ["en"],
           script: ["Latn"],
           title: fetch_titles(hit.hit[:title]),
